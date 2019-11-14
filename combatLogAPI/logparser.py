1c77237456ab93aa62a483bda1ece9014af36f91
CrusaderW <wirth@protectivecircle.com>
2019-11-14 04:39:06 +0100

    parsing completed and jsonified

--- combatLogAPI/logparser.py
+++ combatLogAPI/logparser.py
@@ -4,6 +4,10 @@ from flask import jsonify
 from combatLogAPI import models
 from combatLogAPI.constants import ACTION_TYPES, SKILL_BY_ME, SKILL_TARGET_ME, \
         FOR_SPLITTER, EVENT_SPLITTER, CRITICAL_SUBSTRING
+#TODO: refactor to dataframe siting in RAM globallz accessible
+import numpy as np
+import pandas as pd
+
 
 class LogStream:
 
@@ -19,22 +23,15 @@ class LogStream:
             #try:
                 logLine = LogLine(log['Value'], self.username)
                 parsedLogLine = logLine.parse()
-                #parsedLogStream.append(parsedLogLine)
+                parsedLogStream.append(parsedLogLine)
             #except Exception as e:
             #    print(e)
             #    print('Skipped parsing the following line due to an error.')
                 print(log['Value'])
+                print(parsedLogLine)
                 #TODO: Write unhandled lines to logfile.
-        #response = jsonify(parsedLogStream)
+        response = {'lines_parsed': len(parsedLogStream)}
         #print(response)
-        response = {
-            "friendly":[
-                    {
-                        "username": "GeneralMolan",
-                        "total_damage": 196
-                    }
-            ]
-        }
         return response
 
 class LogLine():
@@ -43,35 +40,49 @@ class LogLine():
         #TODO: Create unified models for JSON and MongoDocument
         self.username = username
         self.log = log
+        self.model = {'columns': ['timestamp',
+                                  'action',
+                                  'source',
+                                  'target',
+                                  'skillName',
+                                  'skillAmount',
+                                  'skillCritical',
+                                  'username']}
         return
 
     def parse(self):
-
-        self.skillAction = self.getSkillAction();
-        #print(self.skillAction)
+        eventPart = self.log.split(EVENT_SPLITTER)[1].strip()[0:-1]
+        self.skillAction = self.getSkillAction(eventPart);
         if self.skillAction is None:
             print("[WARN] line was skipped cause action type not defined\n"+log)
 
-        eventPart = self.log.split(EVENT_SPLITTER)[1].strip()[0:-1]
-        #print(eventPart)
         [skillByAndSkillNamePart,skillTargetAndSkillAmountPart] = self.getSplittedBySkillAction(eventPart)
-        #print([skillByAndSkillNamePart,skillTargetAndSkillAmountPart])
         self.skillBy = self.getSkillBy(skillByAndSkillNamePart)
-        #print(self.skillBy)
         self.skillName = self.getSkillName(skillByAndSkillNamePart)
-        #print(self.skillName)
-        self.dateTime = self.getDateTime();
-        #print(self.dateTime)
+        self.dateTime = self.getDateTime()
         self.skillTarget = self.getSkillTarget(skillTargetAndSkillAmountPart)
-        #print(self.skillTarget)
         self.skillAmount = self.getSkillAmount(skillTargetAndSkillAmountPart)
-        print(self.skillAmount)
-        self.skillCritical = self.isCritical(eventPart);
-        #print(self.skillCritical)
-        return
+        self.skillCritical = self.isCritical(eventPart)
+        self.json = self.get_json()
+        return self.json
 
-    def getSkillAction(self):
-        return 'HIT'
+    def get_json(self):
+        json_data = {}
+        json_data['timestamp'] = self.dateTime
+        json_data['action'] = self.skillAction
+        json_data['source'] = self.skillBy
+        json_data['target'] = self.skillTarget
+        json_data['skillName'] = self.skillName
+        json_data['skillAmount'] = self.skillAmount
+        json_data['skillCritical'] = self.skillCritical
+        json_data['username'] = self.username
+        return json_data
+
+    def getSkillAction(self, eventPart):
+        for actionType in ACTION_TYPES:
+            if (0 < eventPart.find(ACTION_TYPES[actionType])):
+                return actionType
+        return None
 
     def getSplittedBySkillAction(self, eventPart):
         return eventPart.split(ACTION_TYPES[self.skillAction])
--- tests/test_api.py
+++ tests/test_api.py
@@ -8,19 +8,24 @@ json_streamLogs = {
     {
       "Key": "2019-10-23T11:18:33.232Z",
       "Value": "2019-10-23T11:18:33.232Z INFO    COMBAT    - Combat _||_ Event=[Your Vicious Stomp hit Practice Dummy for 196  damage.] "
-    }
+    },
+    {
+      "Key": "2019-10-23T11:10:35.896Z",
+      "Value": "2019-10-23T11:10:35.896Z INFO    COMBAT    - Combat _||_ Event=[Your Strong Swing hit Practice Dummy for 65  damage.] "
+    },
+    {
+      "Key": "2019-10-23T11:10:36.029Z",
+      "Value": "2019-10-23T11:10:36.029Z INFO    COMBAT    - Combat _||_ Event=[Your Strong Swing hit Practice Dummy for 31  damage.] "
+    },
+    {
+      "Key": "2019-10-23T11:10:36.97Z",
+      "Value": "2019-10-23T11:10:36.970Z INFO    COMBAT    - Combat _||_ Event=[Your Crushing Overhead hit Practice Dummy for 324  damage (Critical).] "
+    },
   ]
 }
 
 def test_streamLogs(client):
     response = client.post('/streamLogs', json=json_streamLogs)
     json_response = response.get_json()
-    assert json_response == {
-            "friendly":[
-                {
-                    "username": "GeneralMolan",
-                    "total_damage": 196,
-                }
-            ]
-    }
+    assert json_response == {"lines_parsed":4}
 
